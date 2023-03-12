80886afc7d3912286a33e170b17b5b7a0abbc644
ChristianC244 <christiancattai@gmail.com>
2023-03-12 09:40:15 +0100

    Documented file_reader

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -4,9 +4,13 @@ use chrono::prelude::NaiveDateTime;
 use std::fs::File;
 use std::sync::mpsc::SyncSender;
 
-/// Read a .csv file and send them into a a queue for processing.
+/// Read episodes from a .csv file and send them into a a queue for processing.
 ///
-/// Returns the number of element processed
+/// Returns a Result with the number of episodes read if everything went ok, the error otherwise
+/// # Arguments
+///
+/// * `filepath` - A String with the correct path of seen_episode.csv
+/// * `tx` - The sender from which sends the episodes read for a multithreaded operation
 ///
 pub fn read_episodes(filepath: String, tx: SyncSender<Episode>) -> Result<usize> {
     let file = File::open(filepath)?;
@@ -17,11 +21,11 @@ pub fn read_episodes(filepath: String, tx: SyncSender<Episode>) -> Result<usize>
     for line in reader.records() {
         let record = line.unwrap();
 
-        let season = record.get(2).unwrap().parse::<usize>().unwrap();
-        let number = record.get(3).unwrap().parse::<usize>().unwrap();
-        let tvdb_id = record.get(8).unwrap().parse::<usize>().unwrap();
+        let season = record.get(2).unwrap().parse::<usize>()?;
+        let number = record.get(3).unwrap().parse::<usize>()?;
+        let tvdb_id = record.get(8).unwrap().parse::<usize>()?;
         let watched_at = record.get(0).unwrap();
-        let watched_at = NaiveDateTime::parse_from_str(watched_at, "%Y-%m-%d %H:%M:%S").unwrap();
+        let watched_at = NaiveDateTime::parse_from_str(watched_at, "%Y-%m-%d %H:%M:%S")?;
 
         tx.send(Episode::new(season, number, tvdb_id, watched_at))?;
         counter += 1;
